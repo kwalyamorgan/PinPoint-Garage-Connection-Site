f1fc160ab1df3d4e27eca520b6806e18ceae7d75
Morgan Kwalya <kwalyamorgan042@gmail.com>
2026-07-16 21:48:42 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -17,20 +16,21 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  // Required for GitHub Pages
+  base: '/PinPoint-Garage-Connection-Site/',
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  // File types to support raw imports
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
